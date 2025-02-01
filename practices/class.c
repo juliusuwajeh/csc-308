63a0620f8a9fc906c1bcb4ac576c0861b8794dac
julius.uwajeh <julius.uwajeh@pau.edu.ng>
2025-02-01 23:41:15 +0100

    class practice

---
+++ practices/class.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int age = 99;
+    age = 20;
+    //printf("Basit is %d years old\n", age);
+    //puts("Hello World");
+
+    //char gender = "M";
+    
+    char name[20];
+    printf("Please input your age:");
+    scanf("%d", &age);
+    return 0;
+
+}
